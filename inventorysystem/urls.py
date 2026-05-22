5b6dd2defc90f3463374d6a648ba729500ca63a4
Nicoo Carabeo <nicolocarabeo05@gmail.com>
2026-05-22 12:39:23 +0800

    Initial commit

---
+++ inventorysystem/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for inventorysystem project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from user import views as user_view
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls), 
+    path('', include('dashboard.urls')),
+    path('register/', user_view.register, name='user_register'),
+    path('profile/', user_view.profile, name='user_profile'),
+    path('profile/update/', user_view.profile_update, name='user_profile_update'),
+    path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='user_login'),
+    path('logout/', user_view.user_logout, name='user_logout'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ user/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    staff = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    address = models.CharField(max_length=255, null=True)
+    phone_number = models.CharField(max_length=20, null=True)
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.staff.username} - Profile'
